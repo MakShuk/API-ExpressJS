0cc483c91fdcf565ef415a67008b73bd663ccec9
Maksim Shuklin <makshuk@my.com>
2023-04-08 14:54:57 +0300

    http server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-import { http } from 'http';
+import http from 'http';
+
+const host = '127.0.0.1';
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'GET':
+      switch (req.url) {
+        case '/hellow':
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/plain');
+          res.end('Привет');
+      }
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Сервер запущен на ${host}:${port}`);
+});
