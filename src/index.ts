3b335dd4abe945cb495d06800d99a2c15ccb302a
Maksim Shuklin <makshuk@my.com>
2023-04-08 15:42:17 +0300

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
-import http from 'http';
+import express from 'express';
 
 const host = '127.0.0.1';
 const port = 8000;
 
-const server = http.createServer((req, res) => {
+const app = express();
+app.get('/hollo', (req, res) => {
+  res.send('Привет');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at https://localhost:${port}/`);
+});
+
+
+
+/* const server = http.createServer((req, res) => {
   if (req.method === 'GET' && req.url === '/hello') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Привет');
@@ -16,3 +27,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}/`);
 });
+ */
