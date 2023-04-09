54417923d82bbcf7830ff9d906a4c18982b0c746
Maksim Shuklin <makshuk@my.com>
2023-04-09 11:32:34 +0300

    add error processing

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ app.all('/hello', (req: Request, res: Response, next: NextFunction): void => {
   next();
 });
 
-const cb = (req: Request, res: Response, next: NextFunction) => {
-  console.log('cb');
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log('Time ', Date.now());
   next();
-};
+});
 
 app.use('/users', userRouter);
 
@@ -24,6 +24,17 @@ app
   .post((req: Request, res: Response, next: NextFunction) => {
     res.send('Hi POST');
   });
+
+
+app.use((err: Error, req: Request, res: Response) => {
+  if (err) {
+    console.error(err.message);
+    res.status(500).send("Internal Server Error");
+    return;
+  }
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
