b3e8ca8c481da05c8b06276b8419ba5ea418df8e
Maksim Shuklin <makshuk@my.com>
2023-04-14 07:49:06 +0300

    add LoggerService

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express, { Express } from 'express';
 import { userRouter } from './users/users.js';
 import { Server } from 'http';
+import { LoggerService } from './logger/logger.service.js';
 
 export class App {
   app: Express;
   port: number;
   server: Server;
-  constructor() {
+  logger: LoggerService;
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRoutes() {
@@ -18,6 +21,6 @@ export class App {
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log(`Server running at http://localhost:${this.port}/`);
+    this.logger.log(`Server running at http://localhost:${this.port}/`);
   }
 }
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,21 @@
+import { Logger, ILogObj } from 'tslog';
+
+export class LoggerService {
+  private logger: Logger<ILogObj>;
+
+  constructor() {
+    this.logger = new Logger({ hideLogPositionForProduction: true, });
+  }
+
+  log(...args: unknown[]) {
+    this.logger.info(...args);
+  }
+
+  error(...args: unknown[]) {
+    this.logger.info(...args);
+  }
+
+  warn(...args: unknown[]) {
+    this.logger.warn(...args);
+  }
+}
