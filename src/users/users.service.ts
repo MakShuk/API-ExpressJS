bcc3713194e0b4f87740c72740075503b8bab0d2
Maksim Shuklin <makshuk@my.com>
2023-05-28 09:45:22 +0300

    +UsersRepository

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,19 +5,24 @@ import { User } from './user.entity';
 import { IUserService } from './users.service.interface';
 import { TYPES } from '../types';
 import { IConfigService } from '../config/config.service.interface';
+import { UserModel } from '.prisma/client';
+import { IUsersRepository } from './users.repository.interface';
 
 @injectable()
 export class UserService implements IUserService {
-	constructor(@inject(TYPES.ConfigService) private configService: IConfigService) {}
-	async createUser({ email, name, password }: UserRegisterDto): Promise<User | null> {
+	constructor(
+		@inject(TYPES.ConfigService) private configService: IConfigService,
+		@inject(TYPES.UsersRepository) private usersRepository: IUsersRepository,
+	) {}
+	async createUser({ email, name, password }: UserRegisterDto): Promise<UserModel | null> {
 		const newUser = new User(email, name);
 		const salt = this.configService.get('SALT');
-		console.log(salt);
 		await newUser.setPassword(password, Number(salt));
-		// проверка что он есть?
-		// если есть - возвращаем null
-		// если нет - создаём
-		return null;
+		const existedUser = await this.usersRepository.find(email);
+		if (existedUser) {
+			return null;
+		}
+		return this.usersRepository.create(newUser);
 	}
 	async validateUser(dto: UserLoginDto): Promise<boolean> {
 		return true;
